5db0d0bb12ed99585e8b3726012f9a3b4bd19e37
Elio Bonutto <ebonutto@xmco-ebon.office.xmco.fr>
2026-07-07 17:40:46 +0200

    feat: start parsing

--- src/main.c
+++ src/main.c
@@ -1,46 +1,10 @@
 #include "malcolm.h"
-#include <signal.h> // signal(), SIGINT
-#include <sys/socket.h> // socket(), AF_INET, SOCK_RAW
-#include <arpa/inet.h> // htons()
-#include <stdio.h> // fprintf(), stderr
-#include <unistd.h> // close()
-
-
-// static int g_sig = 1;
-
-
-// int ft_malcolm()
-// {
-// 	return (0);
-// }
-
-
-// static void signal_handler()
-// {
-// 	g_sig = 0;
-// }
-
 
 int main(int argc, char **argv)
 {
-	int sockfd;
-	(void)argv;
-	(void)argc;
-	// if (!parse_input(argc, argv))
-	// 	return (1);
+	t_malcolm_ctx ctx;
 
-	// signal(SIGINT, signal_handler);
-
-	sockfd = socket(AF_INET, SOCK_RAW, htons(ETH_P_ARP));
-	if (sockfd < 0) {
-		fprintf(stderr, "Fuck this\n");
+	if (parse_args(&ctx, argc, argv) != 0)
 		return (1);
-	}
-	while (1) {
-		recvfrom();
-	}
-
-	close(sockfd);
-
-	return (0);
+	return (malcolm(&ctx));
 }
--- src/parse.c
+++ src/parse.c
@@ -1,16 +1,9 @@
 #include "malcolm.h"
-#include <stdio.h> // fprintf(), stderr
 
-void print_usage()
+#include <stdio.h>	// fprintf(), stderr
+
+int parse_args(t_malcolm_ctx *ctx, int argc, char **argv)
 {
-	fprintf(stdout, "Hello\n");
-}
-
-int parse_input(int argc, char **argv)
-{
-	// if (argc > 2 && (ft_strcmp("-h", argv[1]) != 0 || ft_strcmp("--help", argv[1]) != 0)) {
-	// }
-
 	if (argc != 5) {
 		fprintf(stderr, "%s: usage error: invalid number of arguments\n", argv[0]);
 		fprintf(stderr, "\nUsage:\tyeahhh\n");
